b84cfdc796cb1e8d8b35f7633c57b7b7c2158505
VivianChan-TH <63438624+VivianChan-TH@users.noreply.github.com>
2024-05-04 02:01:46 +0800

    opponent board

--- src/client/scripts/game/gameState.js
+++ src/client/scripts/game/gameState.js
@@ -6,6 +6,7 @@ class GameState {
         this.numPlayers = numPlayers;
         this.myBoard = new Board("grid", 30);
         this.myBlock = new Block("type", BLOCK_SHAPES["Z"], 0, 0);
+        this.opponentBoards = {};
 
         // These 2 to be dealt with
         this.myPlayerID = null;
@@ -27,7 +28,14 @@ class GameState {
     }
 
     parseOthersGameState(gameState) {
-        console.log("other players gameState received");
+        const currentPlayerID = gameState["playerID"];
+        if (!(currentPlayerID in this.opponentBoards)) {
+            console.log("other players gameState received");
+            let boardId = `opponent-board-${Object.keys(this.opponentBoards).length + 1}`;
+            this.opponentBoards[currentPlayerID] = new Board(
+                boardId, 14);
+        }
+        this.opponentBoards[currentPlayerID].boardState = gameState["board"];
     }
 
     parseGameState(gameState) {
@@ -42,6 +50,9 @@ class GameState {
         this.myBoard.draw();
         if (this.myBlock != null) {
             this.myBlock.draw();
+        }
+        for (let boardID in this.opponentBoards) {
+            this.opponentBoards[boardID].draw();
         }
         // this.myBlock.draw();
     }
